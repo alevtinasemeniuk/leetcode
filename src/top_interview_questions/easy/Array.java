b00e02759dd2ec639ee8b13ca711f9d83ff35162
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-04-27 19:29:46 -0700

    add rotate method

--- src/top_interview_questions/easy/Array.java
+++ src/top_interview_questions/easy/Array.java
@@ -1,5 +1,7 @@
 package top_interview_questions.easy;
 
+import java.util.Arrays;
+
 public class Array {
     /*
     1. Remove Duplicates from Sorted Array
@@ -64,10 +66,35 @@ public class Array {
         return profit;
     }
 //----------------------------------------------------------------------------------------------------------------------
+    /*
+    3. Rotate Array
 
+    Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 
+    Example:
+    Input: nums = [1,2,3,4,5,6,7], k = 3
+    Output: [5,6,7,1,2,3,4]
+    Explanation:
+    rotate 1 steps to the right: [7,1,2,3,4,5,6]
+    rotate 2 steps to the right: [6,7,1,2,3,4,5]
+    rotate 3 steps to the right: [5,6,7,1,2,3,4]
+    */
 
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int[] result = new int[nums.length];
+        int len = nums.length;
 
+        for (int i = 0; i < k; i++) {
+            result[i] = nums[len - k + i];
+        }
+        for (int i = 0; i < len - k; i++) {
+            result[k + i] = nums[i];
+        }
+        for (int i = 0; i < len; i++) {
+            nums[i] = result[i];
+        }
+    }
 
 
 
@@ -79,5 +106,8 @@ public class Array {
         int[] prices = {7,1,5,3,6,4};
         System.out.println(maxProfit(prices));
 
+        int[] nums = {1,2,3,4,5,6,7};
+        rotate(nums, 3);
+        System.out.println(Arrays.toString(nums));
     }
 }
