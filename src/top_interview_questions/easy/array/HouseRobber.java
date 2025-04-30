fbd14639e0d3210fd43901b705b6be207e8cc080
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-04-29 21:53:49 -0700

    refactor

---
+++ src/top_interview_questions/easy/array/HouseRobber.java
@@ -0,0 +1,21 @@
+package top_interview_questions.easy.array;
+
+public class HouseRobber {
+
+    public int rob(int[] nums) {
+        if (nums.length == 0) {return  0;}
+        if (nums.length == 1) {return nums[0];}
+        if (nums.length == 2) {return Math.max(nums[0], nums[1]);}
+
+        int prev1 = Math.max(nums[0], nums[1]);
+        int prev2 = nums[0];
+        int cur = 0;
+        for (int i = 2; i < nums.length; i++) {
+            cur = Math.max(prev1, prev2 + nums[i]); // i = 2
+
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return cur;
+    }
+}
