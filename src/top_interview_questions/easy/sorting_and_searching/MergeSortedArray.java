d0fb8564dbf5e4f44468e6f59926ee78c3aa4b28
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-05-05 21:15:18 -0700

    refactor

--- src/top_interview_questions/easy/sorting_and_searching/MergeSortedArray.java
+++ src/top_interview_questions/easy/sorting_and_searching/MergeSortedArray.java
@@ -19,6 +19,6 @@ public class MergeSortedArray {
         int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {2,5,6};
         merge(nums1, 3, nums2, 3);
-        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums1)); //[1, 2, 2, 3, 5, 6]
     }
 }
