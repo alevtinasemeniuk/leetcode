8175270adea8030225321987d7c350aeb42e86ce
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-05-12 21:25:47 -0700

    refactor

--- src/problems/MoveZeroes.java
+++ src/problems/MoveZeroes.java
@@ -4,14 +4,16 @@ import java.util.Arrays;
 
 public class MoveZeroes {
     public static void moveZeroes(int[] nums) {
-        int insertPos = 0;
-        for(int num : nums) {
-            if(num != 0) {
-                nums[insertPos++] = num;
+        int index = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
             }
         }
-        while(insertPos < nums.length) {
-            nums[insertPos++] = 0;
+
+        for(int i = index; i < nums.length; i++){
+            nums[i] = 0;
         }
     }
 
